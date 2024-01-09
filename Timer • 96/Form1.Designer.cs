46689fa5bc4c0c90660f1bb99fb84b7ad6768e04
halley <130781977+mshelovee@users.noreply.github.com>
2024-01-09 11:13:45 +0300

    Add files via upload

---
+++ Timer • 96/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Timer___96
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnBaslat = new System.Windows.Forms.Button();
+            this.lblSayac = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnDurdur = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnBaslat
+            // 
+            this.btnBaslat.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnBaslat.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnBaslat.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnBaslat.Location = new System.Drawing.Point(22, 54);
+            this.btnBaslat.Name = "btnBaslat";
+            this.btnBaslat.Size = new System.Drawing.Size(120, 56);
+            this.btnBaslat.TabIndex = 0;
+            this.btnBaslat.Text = "BAŞLAT";
+            this.btnBaslat.UseVisualStyleBackColor = false;
+            this.btnBaslat.Click += new System.EventHandler(this.btnBaslat_Click);
+            // 
+            // lblSayac
+            // 
+            this.lblSayac.AutoSize = true;
+            this.lblSayac.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSayac.Location = new System.Drawing.Point(30, 139);
+            this.lblSayac.Name = "lblSayac";
+            this.lblSayac.Size = new System.Drawing.Size(29, 31);
+            this.lblSayac.TabIndex = 1;
+            this.lblSayac.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnDurdur
+            // 
+            this.btnDurdur.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnDurdur.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnDurdur.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnDurdur.Location = new System.Drawing.Point(148, 54);
+            this.btnDurdur.Name = "btnDurdur";
+            this.btnDurdur.Size = new System.Drawing.Size(120, 56);
+            this.btnDurdur.TabIndex = 2;
+            this.btnDurdur.Text = "DURDUR";
+            this.btnDurdur.UseVisualStyleBackColor = false;
+            this.btnDurdur.Click += new System.EventHandler(this.btnDurdur_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(297, 229);
+            this.Controls.Add(this.btnDurdur);
+            this.Controls.Add(this.lblSayac);
+            this.Controls.Add(this.btnBaslat);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBaslat;
+        private System.Windows.Forms.Label lblSayac;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnDurdur;
+    }
+}
+
